8356f2e8a9f8f3aeb04ed680c9062e28b3856ef5
zavierlau <65946875+zavierlau@users.noreply.github.com>
2025-07-23 17:57:41 +0800

    Update wp-config.php

--- wp/wp-config.php
+++ wp/wp-config.php
@@ -21,22 +21,22 @@
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 if (isset($_ENV['DATABASE'])) {
-  define( 'DB_NAME', $_ENV['test'] );
+  define( 'DB_NAME', $_ENV['DATABASE'] );
 }
 
 /** Database username */
 if (isset($_ENV['USERNAME'])) {
-  define( 'DB_USER', $_ENV['2crV9j11AaJJY7t.root'] );
+  define( 'DB_USER', $_ENV['USERNAME'] );
 }
 
 /** Database password */
 if (isset($_ENV['PASSWORD'])) {
-  define( 'DB_PASSWORD', $_ENV['ttczsH9mbRqn53mc'] );
+  define( 'DB_PASSWORD', $_ENV['PASSWORD'] );
 }
 
 /** Database hostname */
 if (isset($_ENV['HOST'])) {
-  define( 'DB_HOST', $_ENV['gateway01.ap-southeast-1.prod.aws.tidbcloud.com:4000'] );
+  define( 'DB_HOST', $_ENV['HOST'] );
 }
 
 /** Database charset to use in creating database tables. */
